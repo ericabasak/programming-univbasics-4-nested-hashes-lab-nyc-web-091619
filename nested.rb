3f9cec1b46a876d65f25e1fb73aee0bdc2846288
Erica <ericabasak@gmail.com>
2019-09-11 15:04:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -142,6 +142,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:]
+     programmer_hash[:dennis_ritchie][:languages][2] = "Assembly"
+     return programmer_hash
 
 end
